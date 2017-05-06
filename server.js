f4a2fc8d8f4c46b161ab16132fbd9bf73cdfafab
韩朴宇 <w12101111@gmail.com>
2017-05-06 17:18:27 +0800

    login

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 var http = require('http');
 var url = require('url');
-var ws = require('ws');
+var Wss = require('ws').Server;
 var fs = require('fs');
+var Ship = require('./ship.js');
+var Star = require("./star.js");
 
 var mimetype = {
-  'txt': 'text/plain',
-  'html': 'text/html',
-  'css': 'text/css',
-  'xml': 'application/xml',
-  'json': 'application/json',
-  'js': 'application/javascript',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'gif': 'image/gif',
-  'png': 'image/png',
-  'svg': 'image/svg+xml'
-}
+    'txt': 'text/plain',
+    'html': 'text/html',
+    'css': 'text/css',
+    'xml': 'application/xml',
+    'json': 'application/json',
+    'js': 'application/javascript',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'gif': 'image/gif',
+    'png': 'image/png',
+    'svg': 'image/svg+xml'
+};
 
 var wsport = 8888,
     httpport = 8000;
@@ -24,7 +26,7 @@ var wsport = 8888,
 var server = http.createServer(function (req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log(pathname);
-    if(pathname=='/')pathname="/index.html";
+    if (pathname == '/') pathname = "/index.html";
     var realPath = __dirname + '/www' + pathname;
     fs.exists(realPath, function (exists) {
         if (!exists) {
@@ -53,18 +55,87 @@ var server = http.createServer(function (req, res) {
                 }
             });
         }
-    })
+    });
 });
 server.listen(httpport);
-console.log("Server is running at "+ httpport);
+console.log("Server is running at " + httpport);
 
-var wss = new ws.Server({
+var clients = [];
+var data = {};
+var admin;
+
+var wss = new Wss({
     port: wsport
 });
 
+wss.broadcast = function broadcast(msg) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === client.OPEN && typeof client.id != "undefined") {
+            client.send(msg);
+        }
+    });
+};
+
 wss.on('connection', function (ws) {
-    console.log('connected');
+    console.log('connected ,' + (clients.length + 1) + " clients now");
     ws.on('message', function (message) {
-        console.log(message);
-    })
-})
+        if (message == "ping") {
+            ws.send("pong");
+            return;
+        }
+        console.log("received a message:\n" + message);
+        try {
+            msg = JSON.parse(message);
+            if (msg.type == "login") {
+                ws.id = msg.id;
+                ws.uuid = msg.uuid;
+                if (ws.id == "admin") {
+                    if (admin) {
+                        ws.send(JSON.stringify({
+                            type: "exit",
+                            reason: "administrator is existent"
+                        }));
+                    }else{
+                        admin=ws;
+                        ws.isadmin=true;
+                        return;
+                    }
+                }
+                var repeatId = false;
+                for (var i = 0; !repeatId && i < clients.length; i++) {
+                    if (clients[i] && clients[i].id == ws.id) {
+                        repeatId = true;
+                        if (ws.uuid == clients[i].uuid) {
+                            clients[i] = ws;
+                        } else {
+                            ws.send(JSON.stringify({
+                                type: "exit",
+                                reason: "You have the same id with another user"
+                            }));
+                        }
+                    }
+                }
+                if (!repeatId) {
+                    ws.camp = clients.length + 1;
+                    ws.send(JSON.stringify({
+                        type: "camp",
+                        camp: ws.camp
+                    }));
+                    clients.push(ws);
+                }
+                return;
+            }
+            if (msg.type == "mapUpload") {
+                data.map = msg.map;
+                data.maxCamp = msg.map.length;
+                wss.broadcast(JSON.stringify({
+                    type: "mapload",
+                    map: msg.map
+                }));
+            }
+        } catch (e) {
+            console.log(e);
+            console.log(message);
+        }
+    });
+});
---
+++ ship.js
@@ -0,0 +1,61 @@
+function Ship(data, index) {
+    this.x = data[0];
+    this.y = data[1];
+    this.from = data[2];
+    this.to = data[3];
+    this.camp = data[4];
+    this.population = data[5];
+    this.fromX = data[6];
+    this.fromY = data[7];
+    this.index = index;
+}
+Ship.prototype = {
+    move: function () {
+        if (stars[this.to].a) {
+            this.disX = stars[this.to].x - this.fromX;
+            this.disY = stars[this.to].y - this.fromY;
+            var dis0 = Math.sqrt(this.disX * this.disX + this.disY * this.disY);
+            var t = dis0 / config.shipSpeed * config.starspeed;
+            var loc = stars[this.to].move(t);
+            this.disX = loc.x - this.fromX;
+            this.disY = loc.y - this.fromY;
+            this.dis = Math.sqrt(this.disX * this.disX + this.disY * this.disY);
+            this.x += this.disX / this.dis * config.shipSpeed * config.globalSpeed / fps;
+            this.y += this.disY / this.dis * config.shipSpeed * config.globalSpeed / fps;
+            if (distance(this.x, this.y, stars[this.to].x, stars[this.to].y) < 90) {
+                this.arrive();
+            }
+        } else {
+            this.disX = stars[this.to].x - this.fromX;
+            this.disY = stars[this.to].y - this.fromX;
+            this.dis = Math.sqrt(this.disX * this.disX + this.disY * this.disY);
+            this.x += this.disX / this.dis * config.shipSpeed * config.globalSpeed / fps;
+            this.y += this.disY / this.dis * config.shipSpeed * config.globalSpeed / fps;
+            if ((this.x - stars[this.to].x) * this.disX > 0 || (this.y - stars[this.to].y) * this.disY > 0) {
+                this.arrive();
+            }
+        }
+    },
+    shot: function () {
+        this.population -= config.shootSpeed * stars[this.to].size * config.globalSpeed / fps;
+    },
+    arrive: function () {
+        stars[this.to].in(this.population, this.camp);
+        delete ships[this.index];
+    },
+    array: function () {
+        return [this.x,
+            this.y,
+            this.from,
+            this.to,
+            this.camp,
+            this.population,
+            this.fromX,
+            this.fromY
+        ];
+    },
+    frame: function () {
+        this.move();
+    }
+};
+module.exports = Ship;
